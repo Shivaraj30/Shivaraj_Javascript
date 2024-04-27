7ca4506244d7eb28a579bfdff73de6c3176c00eb
Shivaraj30 <42672153+Shivaraj30@users.noreply.github.com>
2024-04-27 13:15:00 +0000

    Datatypes

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,9 @@
-console.log("Shivaraj")
+const accountID = 12145
+let emailID = "shivaraj.m@gmail.com"
+var accountPassword = "215454"
+
+// console.log(accountID)
+
+// accountID = 44545 // can't change const : not allowed
+
+console.table([accountID, emailID, accountPassword])
