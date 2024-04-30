efed7562a270addacfd7e9945ca6fd3b7de9bb32
Shivaraj M <shivarajm@Administrators-Mac-mini.local>
2024-04-30 15:26:40 +0530

    changes

--- Basics/05_String.js
+++ Basics/05_String.js
@@ -1,3 +1,10 @@
 const myString = new String('Shivaraj')
 
-console.log(myString.slice(0,4))
+// console.log(myString.slice(0,4))
+
+
+mailId ="shivaraj.m@techtreeit.com"
+
+const email = mailId.replace("shivaraj.m@techtreeit.com", 'shiv.m@gmail.com')
+
+console.log(email)
