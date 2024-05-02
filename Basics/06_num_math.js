b4e5da32ae8f21fb04b6a2dfe523cf8e98145046
Shivaraj M <shivarajm@Administrators-Mac-mini.local>
2024-05-02 13:50:22 +0530

    changes

--- Basics/06_num_math.js
+++ Basics/06_num_math.js
@@ -1,16 +1,28 @@
-const score = 300
-// console.log(score)
+const score = 400
+// console.log(score);
 
-const balance = new Number(3555555)
-// console.log(balance.toLocaleString('en-IN'))
+const balance = new Number(100)
+// console.log(balance);
 
-const otherNumber = 124.878778
-// console.log(otherNumber.toPrecision(4))
-// console.log(otherNumber.toFixed(2))
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
 
-// ***************Math*********************
+const otherNumber = 123.8966
 
-// console.log(Math.floor(Math.random()*10))
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
 
 const min = 10;
 const max = 20;
